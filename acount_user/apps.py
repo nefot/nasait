8e6df7f83d3ce0d8dd9efedc14e0e7c710bdf4c3
nefot <artyom@efewf>
2024-10-11 10:44:40 +0300

    Modify Structure

---
+++ acount_user/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AcountUserConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'acount_user'
--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'userauth'
+
     'core',
 ]
 
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'djangoProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'vission',
+        'USER': 'mydatabaseuser',
+        'PASSWORD': '_Qwe1Asd2_',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
