7abdd9f59a4c08bb17b146532dedeea3d996d0bf
nefot <artyom27000@mail.ru>
2024-11-04 19:59:53 +0300

    add

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-2q_b1wbp-f-$p&%r66hyt$uupp&-!h&d_3wexp6n3rj!08*53u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['nefoter.pythonanywhere.com']
 
 # Application definition
 
