b099e7257d7bdb5f57b6072a9701896c4bbbf284
nefot <artyom27000@mail.ru>
2024-11-06 02:42:03 +0300

    bugs fix

--- acount_user/views.py
+++ acount_user/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.shortcuts import render
-
+from core.models import SiteSettings
 from core.models import *
 
 
@@ -10,16 +10,16 @@ def home(request):
 
 def specialists(request):
     subsections = SubSection.objects.filter(published=True).order_by('section')
-    return render(request, 'specialists.html', {'subsections': subsections})
+    return render(request, 'specialists.html', {'subsections': subsections,'site_settings': SiteSettings.objects.first()})
 
 def organisation(request):
     subsections = Organization.objects.filter(published=True).order_by('section')
-    return render(request, 'organizations.html', {'subsections': subsections})
+    return render(request, 'organizations.html', {'subsections': subsections,'site_settings': SiteSettings.objects.first()})
 def legal_framework(request):
     subsections = Base.objects.filter(published=True).order_by('section')
-    return render(request, 'legal_framework.html', {'subsections': subsections})
+    return render(request, 'legal_framework.html', {'subsections': subsections,'site_settings': SiteSettings.objects.first()})
 def reference_materials(request):
     subsections = Materials.objects.filter(published=True).order_by('section')
-    return render(request, 'reference_materials.html', {'subsections': subsections})
+    return render(request, 'reference_materials.html', {'subsections': subsections,'site_settings': SiteSettings.objects.first()})
 # news/views.py
 
